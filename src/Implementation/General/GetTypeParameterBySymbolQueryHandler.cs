d0df66a129f50f87e9e5c3f21af3118b725b661b
ErikWe <erikwestberg7@gmail.com>
2024-07-01 20:23:47 +0200

    Use more explicit name for query

---
+++ src/Implementation/General/GetTypeParameterBySymbolQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿namespace Paraminter.Parameters;
+
+using Microsoft.CodeAnalysis;
+
+using System;
+
+/// <summary>Handles <see cref="IGetTypeParameterBySymbolQuery"/>, and responds with <see cref="ITypeParameter"/>.</summary>
+public sealed class GetTypeParameterBySymbolQueryHandler
+    : IQueryHandler<IGetTypeParameterBySymbolQuery, ITypeParameter>
+{
+    /// <summary>Instantiates a <see cref="GetTypeParameterBySymbolQueryHandler"/>, handling <see cref="IGetTypeParameterBySymbolQuery"/>.</summary>
+    public GetTypeParameterBySymbolQueryHandler() { }
+
+    ITypeParameter IQueryHandler<IGetTypeParameterBySymbolQuery, ITypeParameter>.Handle(
+        IGetTypeParameterBySymbolQuery query)
+    {
+        if (query is null)
+        {
+            throw new ArgumentNullException(nameof(query));
+        }
+
+        return new TypeParameter(query.Symbol);
+    }
+
+    private sealed class TypeParameter
+        : ITypeParameter
+    {
+        private readonly ITypeParameterSymbol Symbol;
+
+        public TypeParameter(
+            ITypeParameterSymbol symbol)
+        {
+            Symbol = symbol;
+        }
+
+        ITypeParameterSymbol ITypeParameter.Symbol => Symbol;
+    }
+}
--- src/Queries/IGetTypeParameterBySymbolQuery.cs
+++ src/Queries/IGetTypeParameterBySymbolQuery.cs
@@ -2,8 +2,8 @@
 
 using Microsoft.CodeAnalysis;
 
-/// <summary>Represents a query for a type parameter.</summary>
-public interface IGetTypeParameterQuery
+/// <summary>Represents a query for a type parameter, given the symbol associated with the type parameter.</summary>
+public interface IGetTypeParameterBySymbolQuery
     : IQuery
 {
     /// <summary>The symbol associated with the type parameter.</summary>
